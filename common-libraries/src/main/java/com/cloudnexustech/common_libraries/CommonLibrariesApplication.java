55e08f59b284cdea7688466a793c6850258def42
Kartikey S <kartikeysrivastava@gmail.com>
2024-11-22 22:33:21 +0530

    commit to main

---
+++ common-libraries/src/main/java/com/cloudnexustech/common_libraries/CommonLibrariesApplication.java
@@ -0,0 +1,13 @@
+package com.cloudnexustech.common_libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonLibrariesApplication.class, args);
+	}
+
+}
---
+++ common-libraries/src/test/java/com/cloudnexustech/common_libraries/CommonLibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudnexustech.common_libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
